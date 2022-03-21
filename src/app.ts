f3e2abe888009ed34b6e057062ef1f20b98d0f13
Nikolay Rusev <nikolay.z.rusev@gmail.com>
2022-03-21 17:38:59 +0200

    success using CryptoJS

--- src/app.ts
+++ src/app.ts
@@ -26,19 +26,24 @@ function useCrypto() {
 
 function useCryptoJS() {
     const phpToken = '+wcUaqfSlpuP9CweN5UR3WhOUne41RLfOEzr1bt/aSU=';
-    const key =
-        '9E9CEB8356ED0212C37B4D8CEA7C04B6239175420203AF7A345527AF9ADB0EB8';
+    const testAesKey = Buffer.from(
+        '9E9CEB8356ED0212C37B4D8CEA7C04B6239175420203AF7A345527AF9ADB0EB8',
+        'hex',
+    );
 
     var buf = Buffer.from(phpToken, 'base64');
-    var iv = buf.slice(0, 16).toString('utf8');
-    var ciphertext = buf.slice(16).toString('utf8');
+    var iv = buf.slice(0, 16);
+    var ciphertext = buf.slice(16);
 
-    var DataKey = CryptoJS.enc.Utf8.parse(key);
-    var DataVector = CryptoJS.enc.Utf8.parse(iv.toString());
-    // var DataEncrypt = CryptoJS.enc.Utf8.parse(ciphertext.toString());
-    var decrypted = CryptoJS.AES.decrypt(ciphertext, DataKey, {
-        iv: DataVector,
-    });
+    var DataKey = CryptoJS.enc.Base64.parse(testAesKey.toString('base64'));
+    var DataVector = CryptoJS.enc.Base64.parse(iv.toString('base64'));
+    var decrypted = CryptoJS.AES.decrypt(
+        ciphertext.toString('base64'),
+        DataKey,
+        {
+            iv: DataVector,
+        },
+    );
     var result = CryptoJS.enc.Utf8.stringify(decrypted);
 
     console.log(result);
